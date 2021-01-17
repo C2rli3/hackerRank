164b27db86e396b9611fb1b4e57983ec72ec41cc
Carlie <carliexnyc@Carlies-MacBook-Air.local>
2021-01-17 09:16:15 -0500

    working ideas

--- insertionSortPartOne.js
+++ insertionSortPartOne.js
@@ -1,22 +1,37 @@
 // n = length of array
 // arr =  array to be sorted
 //Carlie Guevara
-//January 6th, 2020
+////January 6th, 2020
 // Jamaica, Queens, New York
+// you could this without using map. 
+// using this to store information and run the task within a microTask recursion
 
 
-function insertionSort1(n, arr){
-                
-//for(let i=0; i<n;i++)
-//{
-//let left = arr[n-(i+2)];
-//let rightecho  = arr[n-(i+1)];
-//!arr[n-(i+2)]<=arr[n-(i+1)] ? arr[n-(i+2)] = arr[n-(i+1)]  : ' ';
-//console.log(arr);
 
+
+
+let n =5;
+let arr = [1,2,4,5,3];
+
+function insertionSort1(n,arr){
+this.store = arr[n];
+this.arr =arr;
 //is the right smaller than the left element? _____left element___ > right____element == true ?? 
-arr.reverse().join().split(',').map(current => console.log(current));
-console.log(arr.reverse());
+arr.reverse().join().split(',').map(current => 
+
+current == undefined ? '' :
+
+queueMicrotask( () => {
+c
+//current == undefined ? '' : th 
 }
+)
+);
+console.log(arr.reverse());
+};
 
 insertionSort1(5,[1,2,4,5,3]);
+
+
+
+
