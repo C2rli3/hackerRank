cd32cf651132d003f4ae62a4cedcdc79703c356c
Carlie <carliexnyc@Carlies-MacBook-Air.local>
2021-01-25 10:08:50 -0500

    new corrected  bonAppetit

--- bonAppetit.js
+++ bonAppetit.js
@@ -2,20 +2,12 @@ function bonAppetit(bill, k, b) {
 //b is the amount of money Anna contributes to the bill
 //k is the index of the item she did not eat
 //bill is an array of all prices
-
 let sum = 0;
-
 bill.map(current => sum+=current);
-
 sum = sum-bill[k];
-
-let change = sum/2;
-
-b==change ? console.log("Bon Appetit") : console.log(truePrice);
-
-console.log(sum, change, bill[k]);
-
-
+let uPay = sum/2;
+let correctPay = Math.abs(b-uPay);
+b==uPay ? console.log("Bon Appetit") : console.log(correctPay);
 }
 
 
